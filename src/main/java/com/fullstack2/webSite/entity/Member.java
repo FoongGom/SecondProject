0ba2c73ca0aaf35a88f63ad9863e095bac04abc1
FoongGom <137484083+FoongGom@users.noreply.github.com>
2023-09-22 18:05:52 +0900

    엔티티
    엔티티들 수정 및 추가

--- src/main/java/com/fullstack2/webSite/entity/Member.java
+++ src/main/java/com/fullstack2/webSite/entity/Member.java
@@ -1,22 +1,44 @@
 package com.fullstack2.webSite.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-@Entity
-@Builder
+
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@ToString
-public class Member extends BaseEntity {
-	@Id
-	private String email;
-	private String password;
-	private String name;
+@Data
+@Entity
+@Builder
+public class Member extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true) 
+    private String email;
+    private String password;
+    private String name;
+    // 주소
+   
+    private String postalCode;
+    private String addressBasic;
+    private String addressRest;
+  
+    private String phone;
+    private String mobile;
+    private String birth;
+    private String role;
+    
+    
+ // 회원가입 시 목록 추가할 곳
+    public static Member createUser(String email, String password, String name, String postalCode, String addressBasic, String addressRest, String phone, String mobile, String birth, PasswordEncoder passwordEncoder) {
+        return new Member(null, email, passwordEncoder.encode(password), name, postalCode, addressBasic, addressRest, phone, mobile, birth,"USER" );
+    }
+
+   
 }
