06774a9e08ca1084ddd60c142afb2a1ae571c31c
FoongGom <137484083+FoongGom@users.noreply.github.com>
2023-09-22 18:06:27 +0900

    레퍼지토리
    추가 및 업데이트

--- src/main/java/com/fullstack2/webSite/repository/MemberRepository.java
+++ src/main/java/com/fullstack2/webSite/repository/MemberRepository.java
@@ -1,11 +1,14 @@
 package com.fullstack2.webSite.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fullstack2.webSite.entity.Member;
 
+import java.util.Optional;
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-
+    Optional<Member> findByEmail(String email);
 }
