b33e21be71b879d004873a1ec9b83b6260133119
FoongGom <137484083+FoongGom@users.noreply.github.com>
2023-09-25 15:21:17 +0900

    컨트롤러 추가 및 수정
    myController 삭제 후 컨트롤러 수정

--- src/main/java/com/fullstack2/webSite/controller/TestController.java
+++ src/main/java/com/fullstack2/webSite/controller/TestController.java
@@ -12,8 +12,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fullstack2.webSite.dtos.QnADTO;
 import com.fullstack2.webSite.dtos.QnAPageRequestDTO;
+import com.fullstack2.webSite.dtos.QnAPageResultDTO;
+import com.fullstack2.webSite.dtos.ReviewDTO;
 import com.fullstack2.webSite.dtos.ReviewPageRequestDTO;
+import com.fullstack2.webSite.dtos.ReviewPageResultDTO;
 import com.fullstack2.webSite.entity.QnA;
 import com.fullstack2.webSite.entity.Review;
 import com.fullstack2.webSite.repository.QnARepository;
@@ -24,46 +28,47 @@ import com.fullstack2.webSite.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequiredArgsConstructor
 public class TestController {
 	
-	@Autowired
 	private final ReviewService reviewService;
-	@Autowired
 	private final ReviewRepository reviewRepository;
-	
-	@Autowired
 	private final QnAService qnaService;
-	@Autowired
 	private final QnARepository qnaRepository;
 	
+	@Autowired // 생성자에 대한 자동 주입 어노테이션
+	public TestController(
+			ReviewService reviewService,
+			ReviewRepository reviewRepository,
+			QnAService qnaService,
+			QnARepository qnaRepository) {
+		this.reviewService = reviewService;
+		this.reviewRepository = reviewRepository;
+		this.qnaService = qnaService;
+		this.qnaRepository = qnaRepository;
+	}
+	
 	@GetMapping("/test")
-	public void test(@ModelAttribute("reviewPageRequestDTO") ReviewPageRequestDTO reviewPageRequestDTO, Model model) {
-		//Model에게 review란에 뿌려질 항목 DTO를 전달함.
-		model.addAttribute("result", reviewService.getList(reviewPageRequestDTO));
+	public void test(@ModelAttribute("reviewPageRequestDTO") ReviewPageRequestDTO reviewPageRequestDTO,
+	                 @ModelAttribute("qnaPageRequestDTO") QnAPageRequestDTO qnaPageRequestDTO,
+	                 Model model) {
+	    // Review 정보와 QnA 정보를 각각 사용하여 작업 수행
+	    ReviewPageResultDTO<ReviewDTO, Review> reviewResult = reviewService.getList(reviewPageRequestDTO);
+	    QnAPageResultDTO<QnADTO, QnA> qnaResult = qnaService.getList(qnaPageRequestDTO);
+	    
+	    // 모델에 결과 추가
+	    model.addAttribute("reviewResult", reviewResult);
+	    model.addAttribute("qnaResult", qnaResult);
 	}
-	
-	@GetMapping("/test/review")
-	public void getReviewData(@ModelAttribute("reviewPageRequestDTO") ReviewPageRequestDTO reviewPageRequestDTO, Model model) {
-		// 리뷰 데이터를 가져오는 메서드
-		model.addAttribute("result", reviewService.getList(reviewPageRequestDTO));
-	}
-	
-	@GetMapping("/test/qna")
-	public void getQnAData(@ModelAttribute("qnaPageRequestDTO") QnAPageRequestDTO qnaPageRequestDTO, Model model) {
-		// QnA 데이터를 가져오는 메서드
-		model.addAttribute("result", qnaService.getList(qnaPageRequestDTO));
-	}
-	
-	@PostMapping("/addReview")
-	public String addReview(@RequestParam String text) {
-		// 현재 로그인한 사용자의 정보를 가져옵니다.
-	    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-	    // 인증 정보에서 사용자 이름을 가져옵니다.
-	    String reviewer = authentication.getName();
-		
-		// 엔티티 생성 및 데이터 저장
+    @PostMapping("/addReview")
+    public String addReview(@RequestParam String text, @ModelAttribute("reviewPageRequestDTO") ReviewPageRequestDTO reviewPageRequestDTO) {
+        // 현재 로그인한 사용자의 정보를 가져옵니다.
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 인증 정보에서 사용자 이름을 가져옵니다.
+        String reviewer = authentication.getName();
+
+        // 엔티티 생성 및 데이터 저장
         Review review = new Review();
         review.setText(text);
         review.setReviewer(reviewer);
@@ -73,11 +78,14 @@ public class TestController {
         // 리뷰 저장
         reviewRepository.save(review);
 
+        // 리뷰를 추가한 후 검색 타입을 다시 설정
+        reviewPageRequestDTO.setType("리뷰");
+
         return "redirect:/test"; // 리다이렉트할 페이지 지정
-	}
-	
+    }
+
 	@PostMapping("/addQnA")
-	public String addQnA(@RequestParam String text) {
+	public String addQnA(@RequestParam String text, @ModelAttribute("qnaPageRequestDTO") QnAPageRequestDTO qnaPageRequestDTO) {
 		//현재 로그인 한 사용자의 정보를 가져옴
 		
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
