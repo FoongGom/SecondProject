ec487df2006e363f0a09633319b278928ab169b5
FoongGom <137484083+FoongGom@users.noreply.github.com>
2023-09-25 15:22:13 +0900

    패키지 추가

---
+++ src/main/java/com/fullstack2/webSite/findPw/MailService.java
@@ -0,0 +1,24 @@
+package com.fullstack2.webSite.findPw;
+
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class MailService {
+   // private JavaMailSender mailSender;
+    private static final String FROM_ADDRESS = "rywlswkd@gmail.com";
+
+  //  public void mailSend(MailDTO mailDto) {
+   //     SimpleMailMessage message = new SimpleMailMessage();
+   //     message.setTo(mailDto.getAddress());
+   //     message.setFrom(MailService.FROM_ADDRESS);
+   //     message.setSubject(mailDto.getTitle());
+   //     message.setText(mailDto.getMessage());
+
+   //     mailSender.send(message);
+  //  }
+}
