05bccd9c9de86a9ca2a328062f84524934ac2a8b
FoongGom <137484083+FoongGom@users.noreply.github.com>
2023-09-27 16:36:23 +0900

    엔티티
    Product(상품) 엔티티추가 및 업데이트

--- src/main/java/com/fullstack2/webSite/entity/BaseEntity.java
+++ src/main/java/com/fullstack2/webSite/entity/BaseEntity.java
@@ -11,14 +11,12 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import lombok.Getter;
 
-
 @MappedSuperclass
 @EntityListeners(value = {AuditingEntityListener.class})
 @Getter
 @Data
 public abstract class BaseEntity {
 
-	//방명록에 작성되는 글들의 날짜값을 자동 지정하도록 컬럼 설정함
 	@CreatedDate//해당 필드 즉 컬럼에 날짜값 자동 반영하도록 선언
 	@Column(name = "regDate", updatable = false)//DB에 regdate 컬럼 생성하도록 선언 및,
 	//값이 처음 insert 이후엔 자동 update 불가 하도록 설정
